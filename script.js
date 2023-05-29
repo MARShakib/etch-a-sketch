b1804a8053fbe2aa637528b0a627d3bf982917a9
Md Ashikur Rahman <arshakib51@gmail.com>
2023-05-30 01:00:53 +0600

    Add black and white features for cell

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const cellContainer = document.querySelector('.cell-container');
 
-for(let i = 0; i < 16 ; i++){
+let numberOfGrid = 16;
+
+for(let i = 0; i < numberOfGrid; i++){
     const row = document.createElement('div');
     row.classList.add('row');
-    for(let j = 0; j < 16; j++){
+    for(let j = 0; j < numberOfGrid; j++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
         row.appendChild(cell);
     }
     cellContainer.appendChild(row);
 }
+
+let currentColor = 'black';
+
+function changeColor(){
+    this.style.backgroundColor = currentColor;
+}
+
+let cells = document.querySelectorAll('.cell');
+cells.forEach(cell => cell.addEventListener('click',changeColor));
+
+const blackButton = document.querySelector('.black');
+const whiteButton = document.querySelector('.white');
+
+blackButton.addEventListener('click', () => {
+    currentColor = 'black';
+});
+
+whiteButton.addEventListener('click', () => {
+    currentColor = 'white';
+});

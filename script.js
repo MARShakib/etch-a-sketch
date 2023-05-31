60f132a54ff34b792d6d5ff68a3dbfdc0859b470
Md Ashikur Rahman <arshakib51@gmail.com>
2023-06-01 03:42:02 +0600

    Add generateCells() function. n x n cells can be generated through console

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const cellContainer = document.querySelector('.cell-container');
 
 let numberOfGrid = 16;
+let cells;
 
-for(let i = 0; i < numberOfGrid; i++){
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for(let j = 0; j < numberOfGrid; j++){
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        row.appendChild(cell);
+generateCells(numberOfGrid);
+
+function generateCells(n){
+    clearCells();
+    for(let i = 0; i < n; i++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for(let j = 0; j < n; j++){
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            row.appendChild(cell);
+        }
+        cellContainer.appendChild(row);
     }
-    cellContainer.appendChild(row);
+    cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.addEventListener('click',changeColor));
 }
 
-// let currentColor = 'black';
+function clearCells(){
+    cellContainer.innerHTML = '';
+}
+
 let blackButtonSelected = true;
 let whiteButtonSelected = false;
 let rainbowButtonSelected = false;
@@ -36,8 +47,7 @@ function getRandomColor(){
     return `rgb(${valueR}, ${valueG}, ${valueB})`;
 }
 
-let cells = document.querySelectorAll('.cell');
-cells.forEach(cell => cell.addEventListener('click',changeColor));
+
 
 const blackButton = document.querySelector('.black');
 const whiteButton = document.querySelector('.white');
